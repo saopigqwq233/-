f3c6f9e2ed7cccefbb20de0dece5c8b79449d435
saopigqwq233 <904139266@qq.com>
2024-07-18 07:59:13 +0800

    code

--- dg/递归/猴子碰撞的方法数.cpp
+++ dg/递归/猴子碰撞的方法数.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
-    long long _pow(int x,int n){
+    long long _myPow(int x, int n){
+        if(n==0)
+            return 1;
 
+        long long halfnum = _myPow(x,n/2);
+        return n%2==0?halfnum*halfnum:halfnum*halfnum*x;
+    }
+    int myPow(long long x, int n) {
+        if (n < 0)
+            return 1.0 / _myPow(x, -n);
+        else
+            return _myPow(x, n);
     }
     int monkeyMove(int n) {
-
+        return myPow(2,n)-2;
     }
 };
