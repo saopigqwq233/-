e383730c7ef776a9d367dd754908cad1ebabcd4e
saopigqwq233 <904139266@qq.com>
2024-07-18 23:14:09 +0800

    code

---
+++ dg/回溯/电话号码回溯.cpp
@@ -0,0 +1,15 @@
+#include "string"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> ret;
+        vector<string> tmp;
+        ret.reserve(11664);
+
+    }
+};
