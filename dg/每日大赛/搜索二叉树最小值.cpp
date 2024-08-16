22c03d3312eaa963729a7e35d6cd93ccfb7386bf
LiuWenhao <115407314+saopigqwq233@users.noreply.github.com>
2024-08-16 22:15:36 +0800

    算法基础

--- dg/每日大赛/搜索二叉树最小值.cpp
+++ dg/每日大赛/搜索二叉树最小值.cpp
@@ -23,29 +23,45 @@ public:
         return min_x>min_y?min_y:min_x;
     }
     int left_right(TreeNode*root){
-        TreeNode*p = root;
+        TreeNode*p = root->left;
         while (p){
-
+            if(p->right){
+                p = p->right;
+            }
+            else
+                return p->val;
         }
+        return p->val;
+    }
+    int right_left(TreeNode*root){
+        TreeNode*p = root->right;
+        while (p){
+            if(p->left){
+                p = p->left;
+            }
+            else
+                return p->val;
+        }
+        return p->val;
     }
     int getMinimumDifference(TreeNode* root) {
         if(root->left&&root->right){//正常情况，只需要比较左子树、右子树和本节点与左子树最右节点，右子树最左节点的差值
             return min_in_four(
-                    abs(root->val-root->left->val),
-                    abs(root->val-root->right->val),
+                    abs(root->val- left_right(root)),
+                    abs(root->val- right_left(root)),
                     getMinimumDifference(root->left),
                     getMinimumDifference(root->right));
         }
         else if(root->left&&!root->right){//左子树在右子树不在
-            return min_in_two(abs(root->val-root->left->val),
+            return min_in_two(abs(root->val- left_right(root)),
                               getMinimumDifference(root->left));
         }
         else if(!root->left&&root->right){
-            return min_in_two(abs(root->val-root->right->val),
+            return min_in_two(abs(root->val- right_left(root)),
                               getMinimumDifference(root->right));
         }
         else
-            return INT_MAX-1;
+            return 100000;
     }
 };
 
