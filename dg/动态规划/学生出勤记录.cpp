3ebfd12e154add39f3173e9f2bf45b08cc9aa992
LiuWenhao <115407314+saopigqwq233@users.noreply.github.com>
2024-08-20 21:09:58 +0800

    算法基础

--- dg/动态规划/学生出勤记录.cpp
+++ dg/动态规划/学生出勤记录.cpp
@@ -32,8 +32,3 @@ public:
         return sum;
     }
 };
-
-作者：力扣官方题解
-        链接：https://leetcode.cn/problems/student-attendance-record-ii/solutions/942386/xue-sheng-chu-qin-ji-lu-ii-by-leetcode-s-kdlm/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
